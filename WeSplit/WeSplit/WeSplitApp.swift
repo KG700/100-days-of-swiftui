cb31feec0bb15c904378ef6410d3962582dffde2
KG700 <karla.gardiner@digital.cabinet-office.gov.uk>
2023-02-10 18:03:05 +0000

    day-17 completed project-01: WeSplit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Karla Gardiner on 10/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
