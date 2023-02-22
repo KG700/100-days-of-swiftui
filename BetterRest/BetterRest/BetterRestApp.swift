ca304e1ec0357c5886c2f63c0dee2f417cd5d1ad
KG700 <karla.gardiner@digital.cabinet-office.gov.uk>
2023-02-22 13:01:10 +0000

    day-27 project-04: BetterRest - created basic components for application

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Karla Gardiner on 21/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
