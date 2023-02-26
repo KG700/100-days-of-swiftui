b2f7918dac3d16c1aeca4e5546b410bf57e78994
KG700 <karla.gardiner@digital.cabinet-office.gov.uk>
2023-02-26 12:31:18 +0000

    day-30 project-05: WordScramble - completed

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Karla Gardiner on 25/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
