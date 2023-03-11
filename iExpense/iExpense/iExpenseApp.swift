2aca21186989eee748042fc36f1a1460a98b5234
KG700 <karla.gardiner@digital.cabinet-office.gov.uk>
2023-03-11 11:57:20 +0000

    day-37 project 07: iExpense - basic functionality completed

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Karla Gardiner on 10/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
