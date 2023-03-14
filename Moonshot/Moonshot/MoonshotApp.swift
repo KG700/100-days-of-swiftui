c24bed9d17d9afe9e752461f385ea3e76b727d0c
KG700 <karla.gardiner@digital.cabinet-office.gov.uk>
2023-03-14 20:24:33 +0000

    day-40 project-08: Moonshot - imported data and created mission view

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Karla Gardiner on 13/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
